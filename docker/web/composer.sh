4dd435c6bb0e465aff5ade3c825e761300fdac7f
Quentin Gillet <42375010+Quentin-Gillet@users.noreply.github.com>
2025-05-05 03:40:15 +0200

    Update composer.sh

--- docker/web/composer.sh
+++ docker/web/composer.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
+
 EXPECTED_SIGNATURE=$(php -r "echo trim(file_get_contents('https://composer.github.io/installer.sig'));")
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
-if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
-then
-    php composer-setup.php --quiet
+if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]; then
+    php composer-setup.php --quiet --version=2.6.5
     RESULT=$?
     rm composer-setup.php
     exit $RESULT
